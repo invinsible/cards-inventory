331052cd15344a8cb521c1db97a95f36b8a8339a
Игорь Сергеев <i.sergeev@teachbase.ru>
2024-04-27 20:33:22 +0300

    resolve cherry-pick

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,10 +1,13 @@
 <script setup>
 import CardsList from '@/components/cards/cardsList/CardsList.vue';
+import Sorting from '@/components/sorting/Sorting.vue';
+import { mockupCards } from '@/const/mockupCards';
 </script>
 
 <template>
   <main class="px-8 py-6">
     <h1 class="font-bold text-xl mb-5">Все карты</h1>
-    <CardsList />
+    <Sorting />
+    <CardsList :decks="mockupCards" />
   </main>
 </template>
