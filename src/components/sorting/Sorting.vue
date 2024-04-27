160540fa74668b017274bbcb0a91367629f28050
Игорь Сергеев <i.sergeev@teachbase.ru>
2024-04-27 21:40:37 +0300

    PP-06: feat: Добавлен watcher за query-параметрами в урл

--- src/components/sorting/Sorting.vue
+++ src/components/sorting/Sorting.vue
@@ -1,25 +1,19 @@
 <script setup>
-import { ref } from 'vue';
-// import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import { sortingList } from '@/const/sorting';
 import CustomSelect from '../UI/CustomSelect/CustomSelect.vue';
 
-const currentSort = ref('+amount');
-// const route = useRoute();
+const route = useRoute();
+const router = useRouter();
 
 function setSort(value) {
-  console.log(value);
+  router.push({ query: { ...route.query, sorting: value } });
 }
 </script>
 
 <template>
   <div class="flex items-top mb-2">
-    <p class="text-sm mr-2">Сортировать: {{ currentSort }}</p>
+    <p class="text-sm mr-2">Сортировать:</p>
     <CustomSelect :options="sortingList" @update:modelValue="setSort" />
-    <!-- <input
-      @input="(event) => (testModel = event.target.value)"
-      :value="testModel"
-      class="border"
-    /> -->
   </div>
 </template>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,12 +2,29 @@
 import CardsList from '@/components/cards/cardsList/CardsList.vue';
 import Sorting from '@/components/sorting/Sorting.vue';
 import { mockupCards } from '@/const/mockupCards';
+import { watch, ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const decks = ref(mockupCards);
+
+watch(
+  () => route.query,
+  (newVal) => {
+    if (newVal.sorting === '+amount') {
+      console.log('сортируем сначал большие');
+    }
+    if (newVal.sorting === '-amount') {
+      console.log('сортируем сначал маленькие');
+    }
+  }
+);
 </script>
 
 <template>
   <main class="px-8 py-6">
     <h1 class="font-bold text-xl mb-5">Все карты</h1>
     <Sorting />
-    <CardsList :decks="mockupCards" />
+    <CardsList :decks="decks" />
   </main>
 </template>
