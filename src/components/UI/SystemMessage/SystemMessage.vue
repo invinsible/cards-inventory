47bca8c242fbb53abe4143c4f3fb03f3c3a53501
Игорь Сергеев <i.sergeev@teachbase.ru>
2024-04-28 15:07:05 +0300

    PP-06: feat: Добавлен загрузка данных с бэкэнда

---
+++ src/components/UI/SystemMessage/SystemMessage.vue
@@ -0,0 +1,21 @@
+<script setup>
+  const props = defineProps({
+    text: String,
+    isDefault: Boolean,
+    isError: Boolean,
+    isSuccess: Boolean,
+  });
+
+  const colors = {
+    'text-red-600 border-red-500': props.isError,
+    'text-green-600 border-green-500': props.isSuccess,
+    'text-slate-600 border-slate-500': props.isDefault
+  };
+
+</script>
+
+<template>
+  <p class="text-sm px-4 py-1 border inline-block" :class="colors">
+    {{ text }}
+  </p>
+</template>
--- src/components/cards/cardsList/CardsList.vue
+++ src/components/cards/cardsList/CardsList.vue
@@ -4,7 +4,7 @@ defineProps(['decks']);
 </script>
 
 <template>
-  <div class="flex items-top gap-4 flex-wrap">
+  <div class="flex items-top gap-4 flex-wrap mb-4">
     <Deck v-for="deck in decks" :key="deck.id" :deck="deck" />
   </div>
 </template>
